5456e45f7034a9a8d5e9ae5d064caad14ddf2d25
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2024-12-08 23:15:26 +0200

    Add LoggerMiddleware and configure it in AppModule
    Implemented LoggerMiddleware to log requests and configured it in AppModule to apply to all routes of UsersController except DELETE and PATCH methods for specific user routes.

--- users/src/app.module.ts
+++ users/src/app.module.ts
@@ -1,12 +1,32 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestMiddleware,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UserModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    // consumer.apply(LoggerMiddleware).forRoutes({ path: 'users', method: RequestMethod.GET });
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude(
+        { path: 'users/:id', method: RequestMethod.DELETE },
+        { path: 'users/:id', method: RequestMethod.PATCH },
+      )
+      .forRoutes(UsersController);
+      // .forRoutes('*');
+  }
+}
